ca15142fe12df95629d64f296e6d38b2877fff5f
engel80 <prettyhip@gmail.com>
2022-08-31 21:40:50 +0900

    .

--- app/build.sh
+++ app/build.sh
@@ -5,14 +5,14 @@ echo "ACCOUNT_ID: $ACCOUNT_ID"
 echo "REGION: $REGION"
 sleep 1
 
-docker build -t eks-terraform .
+docker build -t eks-cdk .
 
-aws ecr create-repository --repository-name eks-terraform --image-scanning-configuration scanOnPush=true --region $REGION
+aws ecr create-repository --repository-name eks-cdk --image-scanning-configuration scanOnPush=true --region $REGION
 
-docker tag eks-terraform:latest ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/eks-terraform:latest
+docker tag eks-cdk:latest ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/eks-cdk:latest
 
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
 
-docker push ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/eks-terraform:latest
+docker push ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/eks-cdk:latest
 
-# docker run -it -p 8080:8080 eks-terraform
+# docker run -it -p 8080:8080 eks-cdk
