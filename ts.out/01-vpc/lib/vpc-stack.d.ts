4fb419a16dc589b000320df7c7e75f21132362dc
engel80 <prettyhip@gmail.com>
2022-08-31 20:22:07 +0900

    .

--- ts.out/01-vpc/lib/vpc-stack.d.ts
+++
@@ -1,6 +0,0 @@
-import { Stack } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { StackCommonProps } from '../../config';
-export declare class VpcStack extends Stack {
-    constructor(scope: Construct, id: string, props: StackCommonProps);
-}
--- ts.out/eks-cluster-nodegroup/lib/cluster-nodegroup-stack.d.ts
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { StackCommonProps } from '../../config';
-/**
- * AmazonSSMManagedInstanceCore role is added to connect to EC2 instance by using SSM on AWS web console
- */
-export declare class EksClusterNodegroupStack extends Stack {
-    constructor(scope: Construct, id: string, props: StackCommonProps);
-}
